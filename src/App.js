a68d31f00564e65040c99b09ddb652f76642fecf
bluetoothman <diegoalonsosanabria@gmail.com>
2022-01-25 14:05:07 -0500

    componentes1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Switch, Route, Routes } from 'react-router-dom'
+import Navbar from './components/navegacion/NavBar';
+import Inicio from './components/paginas/Inicio';
+import Ofertas from './components/paginas/Ofertas'
+import Contacto from './components/paginas/Contacto'
+import ItemListContainer from './components/navegacion/ItemListContainer';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Navbar />
+        <ItemListContainer msg="Hola soy ItemListContainer desde una  prop" />
+
+        <Routes>
+          <Route path='/' exact component={Inicio} />
+          <Route path='/ofertas' component={Ofertas} />
+          <Route path='/contacto' component={Contacto} />
+        </Routes>
+      </Router>
     </div>
   );
 }
